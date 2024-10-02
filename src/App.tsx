2a4c9c3f0f955bea8e8e24318a8081aae1d0c07e
carlosamcruz <carlosamcruz@ufam.edu.br>
2024-10-01 22:53:40 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import crypto from 'crypto';
+export let vcrypto = crypto;
+
 function App() {
+
+  const hash = vcrypto.createHash('sha256');
+  // Update the hash with some data (for example, a string)
+  hash.update('Hello, World!');
+  // Get the resulting hash in hexadecimal format
+  const hashedValue = hash.digest('hex');
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>
+          hash: {hashedValue}
+        </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
